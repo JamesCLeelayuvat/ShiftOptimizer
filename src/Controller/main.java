58b36e71e0aba18a7759ca3acf97132ac5720206
SuperReece <rbdabawss@gmail.com>
2023-11-20 16:49:07 -0800

    Notes Again

--- src/Controller/main.java
+++ src/Controller/main.java
@@ -1,7 +1,8 @@
 package Controller;
 
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         System.out.println("Hello World");
     }
 }
