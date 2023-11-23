d67908a9822f1b3cade12ca37369225a9380c9b5
JamesCLeelayuvat <jmsleelayuvat@gmail.com>
2023-11-22 23:27:26 -0800

    added a bunch of methods for bouncycastle and  jackson

--- src/main/java/Controller/File/Jackson.java
+++ src/main/java/Controller/File/Jackson.java
@@ -3,16 +3,20 @@ package main.java.Controller.File;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import main.java.Controller.Security.BouncyCastle;
 
 import java.io.File;
 import java.lang.reflect.Array;
 
+import static main.java.Controller.Security.BouncyCastle.hashPassword;
+
 public class Jackson {
 
-    private static File getJsonFile(){
+    private static File getJsonFile() {
         File jsonFile = new File("src/main/resources/data.json");
         return jsonFile;
     }
+
     private static JsonNode getRootNode() {
 
         //Getting and returning the root node
@@ -35,20 +39,22 @@ public class Jackson {
         return objectMapper;
     }
 
-    private static ObjectWriter getObjectWriter(){
+    private static ObjectWriter getObjectWriter() {
         ObjectWriter objectWriter = getObjectMapper().writerWithDefaultPrettyPrinter();
         return objectWriter;
     }
+
     public static void addWorker(String username, String password, String status) {
+        //Have to check for duplicate usernames
         JsonNode rootNode = getRootNode();
         ObjectMapper objectMapper = getObjectMapper();
         ObjectWriter objectWriter = getObjectWriter();
-        JsonNode newWorker = objectMapper.createObjectNode().put("username", username).put("password", password).put("status", status);
+        JsonNode newWorker = objectMapper.createObjectNode().put("username", username).put("password", hashPassword(password)).put("status", status);
         ((com.fasterxml.jackson.databind.node.ArrayNode) rootNode.get("workers")).add(newWorker);
 
         try {
             objectWriter.writeValue(getJsonFile(), rootNode);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -56,14 +62,41 @@ public class Jackson {
     public static void removeWorker(String username) {
         JsonNode rootNode = getRootNode();
         ObjectMapper objectMapper = getObjectMapper();
+        ObjectWriter objectWriter = getObjectWriter();
 
         for (int i = 0; i < rootNode.get("workers").size(); i++) {
             if (username.equals(rootNode.get("workers").get(i).get("username").asText())) {
                 System.out.println("Hello");
                 System.out.println(i);
                 ((com.fasterxml.jackson.databind.node.ArrayNode) rootNode.get("workers")).remove(i);
+                try {
+                    objectWriter.writeValue(getJsonFile(), rootNode);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
         }
     }
+
+    public static int getIndexFromUsername(String username) {
+        JsonNode rootNode = getRootNode();
+
+        for (int i = 0; i < rootNode.get("workers").size(); i++) {
+            if (username.equals(rootNode.get("workers").get(i).get("username").asText())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static String getPasswordFromIndex(int i){
+        JsonNode rootNode = getRootNode();
+
+        return rootNode.get("workers").get(i).get("password").asText();
+
+    }
+//    public static boolean checkDuplicateUsername(){
+//
+//    }
 }
--- src/main/java/Controller/main.java
+++ src/main/java/Controller/main.java
@@ -4,12 +4,13 @@ package main.java.Controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import main.java.Controller.File.Jackson;
+import main.java.Controller.Security.BouncyCastle;
 
 import java.io.File;
 
 public class main {
     public static void main(String[] args) {
-
-        Jackson.removeWorker("test1");
+        Jackson.addWorker("test1", "pass1", "ST");
+        BouncyCastle.loginTest("test1", "pass1");
     }
 }
