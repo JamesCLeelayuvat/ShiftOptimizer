15c68597a2bec99121dbec337c978517b1938f89
SuperReece <rbdabawss@gmail.com>
2023-11-24 22:28:05 -0800

    This is the enum system he had me make

---
+++ src/main/java/Model/Schedules/Calender.java
@@ -0,0 +1,13 @@
+package Model.Schedules;
+
+
+import Model.Time.Day;
+
+enum DaysOfTheWeek {Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday}
+
+
+public class Calender
+{
+    DaysOfTheWeek dayOne = DaysOfTheWeek.Monday;
+
+}
--- src/main/java/Model/Time/Day.java
+++ src/main/java/Model/Time/Day.java
@@ -1,5 +1,8 @@
 package main.java.Model.Time;
 
 public class Day {
+
+
+
     Hour[] hours = new Hour[24];
 }
